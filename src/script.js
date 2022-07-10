7c4ab8d206426ff6d00030a033fc0066bf936d3c
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-10 18:12:18 +0200

    Refactor a constructor into a class

--- src/script.js
+++ src/script.js
@@ -18,14 +18,13 @@ addBookForm.addEventListener('submit', e => {
     displayBooks();
 });
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
-    } 
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(e) {
@@ -44,8 +43,7 @@ function deleteBook(e) {
     const index = book.dataset.index;
 
     myLibrary.splice(index, 1);
-
-    displayBooks()
+    displayBooks();
 }
 
 function displayBooks() {
